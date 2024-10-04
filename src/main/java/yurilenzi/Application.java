3b3c20d9687b9890030a42f331b8eabde49b2191
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-10-04 10:03:00 +0200

    Initial commit

---
+++ src/main/java/yurilenzi/Application.java
@@ -0,0 +1,8 @@
+package yurilenzi;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/yurilenzi/classi/Giochi.java
@@ -0,0 +1,4 @@
+package yurilenzi.classi;
+
+public class Giochi {
+}
